784d8cf153a9a0b6fc0299805c623fc81a4d6428
AnoopAG91 <anoopag47@gmail.com>
2023-10-19 09:14:23 +0530

    added mediaquery

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
 import './Header.css'
 import { NavLink } from 'react-router-dom'
+import {AiOutlineClose} from 'react-icons/ai'
+import {RxHamburgerMenu} from 'react-icons/rx'
 
 function Header() {
+  const [isMobile, setIsMobile] = useState(false)
 
   return (
     <div className='header'>
         <div className='logo' >
             <NavLink to={'/'} className="logo" >MovieDB</NavLink>
             </div>
-            <div className='menu'>
+            <div className={isMobile ? 'mob-menu' : 'menu' } onClick={()=> setIsMobile(false)} >
             <NavLink to={'/popular'} activeClassName="active">Popular</NavLink>
             <NavLink to={'/toprated'} activeClassName="active">Top Rated</NavLink>
             <NavLink to={'/upcoming'} activeClassName="active">UpComing</NavLink>
             </div>
-           
-        
+            <button className='toggle-btn' onClick={()=> setIsMobile(!isMobile)}>
+          {isMobile? (<AiOutlineClose/>) : (<RxHamburgerMenu/>)}
+        </button>
     </div>
   )
 }
